730a3d255d472cd35c4afa86b41472135e3e59e6
Imdad316 <imdad.16@gmail.com>
2018-08-18 20:28:00 +0530

    new changes

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () =>{
+	return(
+		<div>
+			<div className='white f3'>
+				{'Imdad, your current rank is ...'}
+			</div>
+			<div className='white f1'>
+				{'#16'}
+			</div>
+		</div>
+		);
+}
+
+export default Rank;
